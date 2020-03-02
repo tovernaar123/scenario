54851d4627ad5fde73190540248da9f49788f8ed
tovernaar123 <56041037+tovernaar123@users.noreply.github.com>
2020-03-02 18:01:19 +0100

    removing all modules
    well that

--- config/pollution_grading.lua
+++
@@ -1,9 +0,0 @@
---- This controls how pollution is viewed on the map
--- @config Pollution-Grading
-
-return {
-    reference_point = {x=0,y=0}, --- @setting reference_point where pollution is read from
-    max_scalar = 0.5, --- @setting max_scalar the scale between true max and max
-    min_scalar = 0.17, --- @setting min_scalar the scale between the lowest max and min
-    update_delay = 15 --- @setting update_delay time in minutes between view updates
-}
