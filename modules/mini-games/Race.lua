4cf1955038cf93696c25a754b49483e48d44bf79
tovernaar123 <56041037+tovernaar123@users.noreply.github.com>
2020-05-07 18:12:17 +0200

    lines

--- modules/mini-games/Race.lua
+++ modules/mini-games/Race.lua
@@ -2,9 +2,17 @@ local Mini_games = require 'expcore.Mini_games'
 local surface
 local start  = function ()
     surface = game.surfaces["Race game"]
-    local car =  surface.create_entity{name="car", position={-80, -140},force=game.connected_players[1].force}
-    car.set_driver(game.connected_players[1])
-
+    local done = 0
+    for i, player in ipairs(game.connected_players) do
+        local pos
+        if (done % 2 == 0) then
+            pos = {-85, -132+done*5}
+        else
+            pos = {-75,132+done*5}
+        end
+        local car =  surface.create_entity{name="car", position=pos,force=game.connected_players[1].force}
+        car.set_driver(game.connected_players[1])
+    end
 
 end
 
