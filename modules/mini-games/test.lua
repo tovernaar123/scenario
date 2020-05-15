d2ed225f81d073c1a3a172f1f7ae7d420d7a718d
tovernaar123 <56041037+tovernaar123@users.noreply.github.com>
2020-05-15 17:20:15 +0200

    Delete test.lua

--- modules/mini-games/test.lua
+++
@@ -1,6 +0,0 @@
-local surface = game.player.surface
-local entities = surface.find_entities_filtered{area = area}
-for k, wall in pairs(entities) do
-    wall.minable  =false 
-    wall.destructible = false
-end
